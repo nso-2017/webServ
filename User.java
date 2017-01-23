49b2c1e78f867640ce7972b2a7d68b35c75d6987
nso-2017 <n.s.oborin@gmail.com>
2017-01-23 17:15:16 +0100

    Create User.java

---
+++ User.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Created by noborin on 23-1-2017.
+ */
+public class User{
+    public String userName;
+    public String password;
+
+    public User(String usr, String pass){
+        userName = usr;
+        password = pass;
+    }
+}
